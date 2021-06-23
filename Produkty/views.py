e3d8762c4057ab7ed0338e343f8b91c0e1a92dca
Sebastian <sebastian.trupinda@interia.pl>
2021-06-23 15:45:32 +0200

    'podstawa'

---
+++ Produkty/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Produkty, Kategoria, Zdj
+
+
+# Create your views here.
+
+def index(request):
+    kategorie = Kategoria.objects.all()
+    dane = {'kategorie': kategorie}
+    return render(request, 'szablon.html', dane)
+
+
+def kategoria(request, id):
+    kategoria_user = Kategoria.objects.get(pk=id)
+    kategoria_produkt = Produkty.objects.filter(kategoria=kategoria_user)
+    kategorie = Kategoria.objects.all()
+    dane = {'kategoria_user': kategoria_user,
+            'kategoria_produkt': kategoria_produkt,
+            'kategorie': kategorie}
+    return render(request, 'kategoria_produkt.html', dane)
+
+
+def produkt(request, id):
+    kategorie = Kategoria.objects.all()
+    produkt_user = Produkty.objects.get(pk=id)
+    dane = {'produkt_user': produkt_user, 'kategorie': kategorie}
+    return render(request, 'produkt.html', dane)
+
+
+def zdj(request, id):
+    kategorie = Kategoria.objects.all()
+    zdjecie_user = Zdj.objects.get(pk=id)
+    dane = {'zdjecie_user': zdjecie_user, 'kategorie': kategorie}
+    return render(request, 'produkt.html', dane)
